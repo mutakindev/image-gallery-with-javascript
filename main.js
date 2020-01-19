5cbb878f69891089ed22453471be150671ef9ffd
mutakindev <mutakin.ind@gmail.com>
2020-01-19 22:52:26 +0700

    make image gallery with javascript

---
+++ main.js
@@ -0,0 +1,22 @@
+// // cara #1
+const jumbo = document.querySelector('.jumbo');
+// const thumbs = document.querySelectorAll('.thumb');
+
+// thumbs.forEach(el => {
+//     el.addEventListener('click',function(e) {
+//        jumbo.setAttribute('src',e.srcElement.src);
+//     })
+// });
+
+// cara #2
+const container = document.querySelector('.container');
+
+container.addEventListener('click',function(e) {
+    if(e.target.className == 'thumb'){
+        jumbo.src = e.target.src;
+        jumbo.classList.add('fade');
+        setTimeout(function() {
+            jumbo.classList.remove('fade');
+        },500)
+    }
+})
